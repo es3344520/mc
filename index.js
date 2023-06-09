575911891ea4969bc13974c4b43de5caae007e16
es3344520 <74056738+es3344520@users.noreply.github.com>
2023-06-09 23:40:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const app = express();
 
 const TOKEN_PATH = "/token-auth";
@@ -7,7 +6,6 @@ const COOKIE_PATH = "/cookie-auth";
 const COOKIE_NAME = "p-b";
 
 app.use(express.json());
-app.use(cookieParser());
 
 // Token authentication middleware
 app.use(TOKEN_PATH, (req, res, next) => {
@@ -21,26 +19,26 @@ app.use(TOKEN_PATH, (req, res, next) => {
   }
 });
 
+// Cookie authentication middleware
+app.use(COOKIE_PATH, (req, res, next) => {
+  const clientCookie = req.cookies[COOKIE_NAME];
+  const serverCookie = process.env.COOKIE_SECRET;
+
+  if (clientCookie && clientCookie === serverCookie) {
+    next();
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
+
 // Token authentication route
 app.get(TOKEN_PATH, (req, res) => {
   res.send('Token authentication successful');
 });
 
-// Cookie route without authentication
+// Cookie authentication route
 app.get(COOKIE_PATH, (req, res) => {
-  const cookieValue = process.env.COOKIE_SECRET;
-  res.send(`Cookie value from environment variable: ${cookieValue}`);
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-// Set cookie route
-app.get(SET_COOKIE_PATH, (req, res) => {
-  res.cookie(COOKIE_NAME, process.env.COOKIE_SECRET, { httpOnly: true });
-  res.send('Cookie set successfully');
+  res.send('Cookie authentication successful');
 });
 
 const port = process.env.PORT || 3000;
